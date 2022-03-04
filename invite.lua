af268e3956e5406404cc8dc3622c63033b128fd9
shib <98693685+eosuwu@users.noreply.github.com>
2022-03-04 20:24:23 +0100

    Create invite.lua

---
+++ invite.lua
@@ -0,0 +1,32 @@
+local req = request or http_request or (syn and syn.request)
+if req then
+local event = Instance.new("BindableFunction")
+event.OnInvoke = function(answer)
+    if answer == "Yes" then
+    req({
+    Url = "http://127.0.0.1:6463/rpc?v=1",
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json",
+        ["Origin"] = "https://discord.com"
+    },
+    Body = HS:JSONEncode({
+        cmd = "INVITE_BROWSER",
+        args = {
+            code = "nSEbfnvtfF",
+        },
+        nonce = game:GetService("HttpService"):GenerateGUID(false)
+    }),
+    })
+end
+game.StarterGui:SetCore("SendNotification",  {
+ Title = "Discord Server",
+ Text = "Would you like to join our Discord server?";
+ Icon = "";
+ Duration = 5;
+ Button1 = "Yes";
+ Button2 = "Nope";
+ Callback = event;
+})
+    
+end
